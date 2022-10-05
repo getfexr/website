3f33c9e259491001ddfd94014fd347378ac4e391
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2022-10-06 00:58:49 +0530

    Modify timeline to accept generic arrays

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+const timelineData = [
+  {
+    title: 'Fexr is born',
+    description: 'Fexr is born',
+    date: '2021 January',
+  },
+  {
+    title: 'Fexr is born',
+    description: 'Fexr is born',
+    date: '2021 January',
+  },
+];
+
 export default function Timeline() {
   return (
     <section>
-      <div className="py-8">
-        <div className="container mx-auto flex flex-col items-start xl:flex-row my-12 md:my-24">
+      <div>
+        <div className="container mx-auto flex flex-col items-start xl:flex-row">
           <div className="flex flex-col w-full sticky xl:top-36 xl:w-1/3 mt-2 xl:mt-12 px-8">
             <p className="ml-2 text-yellow-300 uppercase tracking-loose">Working Process</p>
             <p className="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2">Working Process of Fest</p>
@@ -39,54 +52,36 @@ export default function Timeline() {
                     borderRadius: '1%',
                   }}
                 />
-                <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                  <div className="order-1 w-5/12" />
-                  <div className="order-1 w-5/12 px-1 py-4 text-right">
-                    <p className="mb-3 text-base text-yellow-300">1-6 May, 2021</p>
-                    <h4 className="mb-3 font-bold text-lg md:text-2xl">Registration</h4>
-                    <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                      Pick your favourite event(s) and register in that event by filling the form corresponding to that
-                      event. Its that easy :)
-                    </p>
-                  </div>
-                </div>
-                <div className="mb-8 flex justify-between items-center w-full right-timeline">
-                  <div className="order-1 w-5/12" />
-                  <div className="order-1  w-5/12 px-1 py-4 text-left">
-                    <p className="mb-3 text-base text-yellow-300">6-9 May, 2021</p>
-                    <h4 className="mb-3 font-bold text-lg md:text-2xl">Participation</h4>
-                    <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                      Participate online. The links for your registered events will be sent to you via email and whatsapp
-                      groups. Use those links and show your talent.
-                    </p>
-                  </div>
-                </div>
-                <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                  <div className="order-1 w-5/12" />
-                  <div className="order-1 w-5/12 px-1 py-4 text-right">
-                    <p className="mb-3 text-base text-yellow-300"> 10 May, 2021</p>
-                    <h4 className="mb-3 font-bold text-lg md:text-2xl">Result Declaration</h4>
-                    <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                      The ultimate genius will be revealed by our judging panel on 10th May, 2021 and the resukts will be
-                      announced on the whatsapp groups and will be mailed to you.
-                    </p>
-                  </div>
-                </div>
-
-                <div className="mb-8 flex justify-between items-center w-full right-timeline">
-                  <div className="order-1 w-5/12" />
-
-                  <div className="order-1  w-5/12 px-1 py-4">
-                    <p className="mb-3 text-base text-yellow-300">12 May, 2021</p>
-                    <h4 className="mb-3 font-bold  text-lg md:text-2xl text-left">Prize Distribution</h4>
-                    <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                      The winners will be contacted by our team for their addresses and the winning goodies will be sent at
-                      their addresses.
-                    </p>
-                  </div>
-                </div>
+                {timelineData.map((data, idx) => {
+                  const type = idx % 2 === 0 ? 'left' : 'right';
+                  const classNames = {
+                    main: {
+                      left: 'flex-row-reverse',
+                      right: 'flex-row',
+                    },
+                    child: {
+                      left: 'text-right',
+                      right: 'text-left',
+                    },
+                  };
+                  return (
+                    <div className={
+                    `mb-8 flex justify-between items-center w-full ${classNames.main[type]}`
+                    }
+                    >
+                      <div className="order-1 w-5/12" />
+                      <div className={ `order-1 w-5/12 px-1 py-4 ${classNames.child[type]}`}>
+                        <p className="mb-3 text-base text-yellow-300">{ data.date }</p>
+                        <h4 className="mb-3 font-bold text-lg md:text-2xl">{ data.title }</h4>
+                        <p className="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
+                          { data.description }
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
-              <img className="mx-auto -mt-36 md:-mt-36" src="https://user-images.githubusercontent.com/54521023/116968861-ef21a000-acd2-11eb-95ac-a34b5b490265.png" />
+              <img className="mx-auto" src="https://user-images.githubusercontent.com/54521023/116968861-ef21a000-acd2-11eb-95ac-a34b5b490265.png" />
             </div>
           </div>
         </div>
