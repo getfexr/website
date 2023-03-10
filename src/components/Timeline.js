42f41be9957362c95b4d59a10a08e1c09626455d
Nidhin Mahesh A <nidhinmahesh1@gmail.com>
2023-03-10 11:50:40 +0530

    commenting roadmap descriptions

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const timelineData = [
   {
-    title: 'Fluir (upcoming)',
+    title: 'Fluir (latest stable release)',
     description:
       'We\'re excited to announce the upcoming release of Fexr\'s third iteration, Fluir, which will soon be available for download on the App Store and Google Play. With Fluir, we\'re introducing an exciting new feature that will make it easier than ever to manage your finances on the go.\n Now, you can connect your Fexr wallet with popular web and mobile apps, allowing for seamless integration between your financial tools and your favorite apps. With just a few clicks, you\'ll be able to connect your wallet and sign transactions from your phone, without ever having to leave the app you\'re using.\n We chose the name Fluir for our upcoming release because it represents the fluidity and ease-of-use that we strive for in all of our products. Fluir, which translates to "flow" in Spanish and Portuguese, perfectly captures the seamless and intuitive experience we\'re aiming to provide our users. With Fluir, managing your finances on the go will feel effortless and intuitive, allowing you to focus on what matters most - your financial goals. \n At Fexr, we\'re committed to providing our users with the most innovative and secure financial tools on the market. We\'re confident that Fluir will provide a truly unparalleled experience, making it easier than ever to manage your finances on the go. Stay tuned for more information on Fluir and all the exciting features we have in store for future Fexr releases!',
     date: '2023 March',
   },
   {
-    title: 'Hypergreen (stable)',
+    title: 'Hypergreen',
     description: 'We\'re thrilled to announce the second release of Fexr, now available for download on both the App Store and Google Play! Our latest release, Hypergreen (stable), is a major step forward in our ongoing mission to provide innovative financial and security tools to our users.\n With Hypergreen, we\'ve taken the opportunity to refresh our wallet app with a brand-new design, making it easier than ever to manage your finances on the go. We\'ve also introduced an exciting new reward program, giving our users even more incentives to use Fexr for their financial needs.\n But that\'s not all - we\'ve also revamped our onboarding process, making it simpler and more intuitive than ever before. Whether you\'re a seasoned Fexr user or just getting started, our new onboarding process will guide you through every step of the way, making it easy to take full advantage of everything Fexr has to offer.\nAt Fexr, our commitment to providing exceptional financial and security tools is at the forefront of everything we do. We\'re confident that Hypergreen will provide users with an unparalleled experience, and we look forward to continuing to innovate and improve our platform with each new release.',
     date: '2022 Oct',
   },
@@ -86,9 +86,10 @@ export default function Timeline() {
                         <h4 className="mb-3 font-bold text-lg md:text-2xl">
                           {data.title}
                         </h4>
-                        <p className="text-sm md:text-base leading-snug text-gray-500 dark:text-gray-50 text-opacity-100">
+                        {/* <p className="text-sm md:text-base leading-snug text-gray-
+                        500 dark:text-gray-50 text-opacity-100">
                           {data.description}
-                        </p>
+                        </p> */}
                       </div>
                     </div>
                   );
