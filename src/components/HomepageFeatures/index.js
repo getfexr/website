e5ab8af7adba22c923d842061de99860dcb3e6a1
nidhinmahesh <nidhinmahesh1@gmail.com>
2023-11-02 03:04:57 +0530

    converting feature section to SDK section

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,90 +1,198 @@
 /* eslint-disable max-len */
 /* eslint-disable react/jsx-indent */
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useBaseUrlUtils } from '@docusaurus/useBaseUrl';
 // import { InlineLink } from '@algolia/ui-library';
 
 import styles from './styles.module.css';
 
-function FeatureSection() {
+function SDK() {
+  const [activeSection, setActiveSection] = useState('Python');
+
+  const sections = [
+    {
+      name: "Python",
+      image: "https://dummyimage.com/720x600",
+      title: "Value Stream Mapping for the Modern Age",
+      description:
+        "A structured approach to visualize communications and information flow...",
+      link: "/process-maps",
+      icon: (
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-5 h-5 mr-3"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12.5,2.5v4c0,0.3-0.2,0.5-0.5,0.5h-4C7.7,7,7.5,6.8,7.5,6.5v-4C7.5,2.2,7.7,2,8,2h4C12.3,2,12.5,2.2,12.5,2.5z M11.5,3h-3v3h3V3z"></path>
+          <path d="M15.5,2.5v4c0,0.3,0.2,0.5,0.5,0.5h4c0.3,0,0.5-0.2,0.5-0.5v-4c0-0.3-0.2-0.5-0.5-0.5h-4C15.7,2,15.5,2.2,15.5,2.5z M16.5,3h3v3h-3V3z"></path>
+          <path d="M11.5,21.5v-4c0-0.3,0.2-0.5,0.5-0.5h4c0.3,0,0.5,0.2,0.5,0.5v4c0,0.3-0.2,0.5-0.5,0.5h-4C11.7,22,11.5,21.8,11.5,21.5z M12.5,21h3v-3h-3V21z"></path>
+          <path d="M8.5,21.5v-4c0-0.3-0.2-0.5-0.5-0.5h-4C3.7,17,3.5,17.2,3.5,17.5v4c0,0.3,0.2,0.5,0.5,0.5h4C8.3,22,8.5,21.8,8.5,21.5z M7.5,21h-3v-3h3V21z"></path>
+        </svg>
+      ),
+    },
+    {
+      name: "Flutter",
+      image: "https://dummyimage.com/720x600",
+      title: "Ensuring Safety in Crowded Areas",
+      description: "Advanced techniques to maintain crowd protection...",
+      link: "/crowd-protection",
+      icon: (
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-5 h-5 mr-3"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+        </svg>
+      ),
+    },
+    {
+      name: "Javascript",
+      image: "https://dummyimage.com/720x600",
+      title: "Ensuring Safety in Crowded Areas",
+      description: "Advanced techniques to maintain crowd protection...",
+      link: "/crowd-protection",
+      icon: (
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-5 h-5 mr-3"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+        </svg>
+      ),
+    },
+    {
+      name: "Endpoint",
+      image: "https://dummyimage.com/720x600",
+      title: "Ensuring Safety in Crowded Areas",
+      description: "Advanced techniques to maintain crowd protection...",
+      link: "/crowd-protection",
+      icon: (
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-5 h-5 mr-3"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+        </svg>
+      ),
+    },
+    // ... add other sections in similar format
+  ];
+
   return (
-    <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto flex flex-wrap flex-col">
-        <div class="flex mx-auto flex-wrap mb-20">
-          <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium bg-gray-100 inline-flex items-center leading-none border-indigo-500 text-indigo-500 tracking-wider rounded-t">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-5 h-5 mr-3"
-              viewBox="0 0 24 24"
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto flex flex-wrap flex-col">
+        <div className="flex mx-auto flex-wrap mb-20">
+          {sections.map((section) => (
+            <a
+              key={section.name}
+              onClick={() => setActiveSection(section.name)}
+              className={`sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none tracking-wider rounded-t ${
+                activeSection === section.name
+                  ? "bg-gray-100 border-indigo-500 text-indigo-500"
+                  : "border-gray-200 hover:text-gray-900 hover:border-indigo-500"
+              }`}
             >
-              <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-            </svg>
-            Process Maps
-          </a>
-          <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-5 h-5 mr-3"
-              viewBox="0 0 24 24"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-            </svg>
-            Onboarding
-          </a>
-          <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-5 h-5 mr-3"
-              viewBox="0 0 24 24"
-            >
-              <circle cx="12" cy="5" r="3"></circle>
-              <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
-            </svg>
-            Open Loyality
-          </a>
-          <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-5 h-5 mr-3"
-              viewBox="0 0 24 24"
-            >
-              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-              <circle cx="12" cy="7" r="4"></circle>
-            </svg>
-            Micro Listings
-          </a>
+              {section.icon}
+              {section.name}
+            </a>
+          ))}
         </div>
-        <img
-          class="xl:w-1/4 lg:w-1/3 md:w-1/2 w-2/3 block mx-auto mb-10 object-cover object-center rounded"
-          alt="hero"
-          src="https://dummyimage.com/720x600"
-        />
-        <div class="flex flex-col text-center w-full">
-          <h1 class="text-xl font-medium title-font mb-4 text-gray-900">
-            Value Stream Mapping for the Modern Age: A Decentralized Vision
-          </h1>
-          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
-            a structured approach to visualize communications and information
-            flow with or without using blockchain in the case of using Fexr
-          </p>
+        {sections.map(
+          (section) =>
+            activeSection === section.name && (
+              <div key={section.name} className="flex flex-col items-center">
+                <img
+                  className="xl:w-1/4 lg:w-1/3 md:w-1/2 w-2/3 mb-10 object-cover object-center rounded"
+                  alt={section.name}
+                  src={section.image}
+                />
+                <div class="flex flex-wrap -m-12">
+      <div class="p-12 md:w-1/2 flex flex-col items-start">
+        <span class="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest">API</span>
+        <h2 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">Roof party normcore before they sold out, cornhole vape</h2>
+        <p class="leading-relaxed mb-8">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric. Cray pug you probably haven't heard of them hexagon kickstarter craft beer pork chic.</p>
+        <div class="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
+          <a class="text-indigo-500 inline-flex items-center">Try It Out
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+          <span class="text-gray-400 mr-3 inline-flex items-center ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+              <circle cx="12" cy="12" r="3"></circle>
+            </svg>1.2K
+          </span>
+          <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+              <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+            </svg>6
+          </span>
         </div>
+        <a class="inline-flex items-center">
+          <img alt="blog" src="https://dummyimage.com/104x104" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
+          <span class="flex-grow flex flex-col pl-4">
+            <span class="title-font font-medium text-gray-900">Holden Caulfield</span>
+            <span class="text-gray-400 text-xs tracking-widest mt-0.5">UI DEVELOPER</span>
+          </span>
+        </a>
+      </div>
+      <div class="p-12 md:w-1/2 flex flex-col items-start">
+        <span class="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest">API</span>
+        <h2 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">Pinterest DIY dreamcatcher gentrify single-origin coffee</h2>
+        <p class="leading-relaxed mb-8">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric.</p>
+        <div class="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
+          <a class="text-indigo-500 inline-flex items-center">Try It Out
+            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+          <span class="text-gray-400 mr-3 inline-flex items-center ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+              <circle cx="12" cy="12" r="3"></circle>
+            </svg>1.2K
+          </span>
+          <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+            <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+              <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+            </svg>6
+          </span>
+        </div>
+        <a class="inline-flex items-center">
+          <img alt="blog" src="https://dummyimage.com/103x103" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"/>
+          <span class="flex-grow flex flex-col pl-4">
+            <span class="title-font font-medium text-gray-900">Alper Kamu</span>
+            <span class="text-gray-400 text-xs tracking-widest mt-0.5">DESIGNER</span>
+          </span>
+        </a>
+      </div>
+    </div>
+              </div>
+            )
+        )}
       </div>
     </section>
   );
@@ -188,7 +296,7 @@ function TransparentEvolution() {
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
               <span class="title-font font-medium">
-                Authentic Cliche Forage
+                Crowd Protection
               </span>
             </div>
           </div>
@@ -207,7 +315,7 @@ function TransparentEvolution() {
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
               <span class="title-font font-medium">
-                Kinfolk Chips Snackwave
+                Web3 User Onboarding
               </span>
             </div>
           </div>
@@ -225,7 +333,7 @@ function TransparentEvolution() {
                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
-              <span class="title-font font-medium">Coloring Book Ethical</span>
+              <span class="title-font font-medium">Web3 Wallet Checkout</span>
             </div>
           </div>
           <div class="p-2 sm:w-1/2 w-full">
@@ -243,7 +351,7 @@ function TransparentEvolution() {
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
               <span class="title-font font-medium">
-                Typewriter Polaroid Cray
+                Process Maps
               </span>
             </div>
           </div>
@@ -261,7 +369,7 @@ function TransparentEvolution() {
                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
-              <span class="title-font font-medium">Pack Truffaut Blue</span>
+              <span class="title-font font-medium">Open Loyality</span>
             </div>
           </div>
           <div class="p-2 sm:w-1/2 w-full">
@@ -278,7 +386,7 @@ function TransparentEvolution() {
                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
-              <span class="title-font font-medium">The Catcher In The Rye</span>
+              <span class="title-font font-medium">Compliance Info</span>
             </div>
           </div>
         </div>
@@ -331,7 +439,7 @@ function Pricing() {
                     <path d="M20 6L9 17l-5-5"></path>
                   </svg>
                 </span>
-                Vexillologist pitchfork
+                Create upto 2 Clubs
               </p>
               <p class="flex items-center text-gray-600 mb-2">
                 <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -410,7 +518,7 @@ function Pricing() {
                     <path d="M20 6L9 17l-5-5"></path>
                   </svg>
                 </span>
-                Vexillologist pitchfork
+                Priced per Club
               </p>
               <p class="flex items-center text-gray-600 mb-2">
                 <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -709,7 +817,7 @@ export default function HomepageFeatures() {
       <div className="w-full">
         <TransparentEvolution />
         <SetupDemo />
-        <FeatureSection />
+        <SDK />
         <Pricing/>
       </div>
     </section>
